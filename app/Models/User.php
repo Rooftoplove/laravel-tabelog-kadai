8121c405825a82841c98e16d8d8e5a5606854c27
Rooftoplove <kamekazu910@yahoo.co.jp>
2024-01-24 19:23:18 +0900

    属性作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,4 +69,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Reservation::class);
     }
+
+    public function getIsPaidMemberAttribute()
+    {
+        return $this->attributes['member_status'] == 'paid';
+    }
 }
