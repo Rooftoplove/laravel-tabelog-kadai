2d0ac220c614fd17453c33737597c1df25a5977f
Rooftoplove <kamekazu910@yahoo.co.jp>
2024-02-09 21:03:25 +0900

    　予約日選択修正

--- resources/views/reservations/subscription.blade.php
+++ resources/views/reservations/subscription.blade.php
@@ -20,8 +20,10 @@
       <h2>{{$restaurant->store_name}}</h2>
       <form method="POST" action="{{route('reservations.register')}}">
         @csrf
-        <label for="date">予約日:</label>
-        <input type="date" name="date" required><br>
+        <div id="app">
+          <label for="date">予約日:</label>
+          <input type="date" name="date" v-bind:min="today" required><br>
+        </div>
         <label for="time">予約時間:</label>
         <input type="time" name="time" min="11:00" max="23:00" required><br>
         <label for="people">予約人数:</label>
@@ -32,6 +34,23 @@
       </form>
     </div>
   </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+  <script>
+    var vue = new Vue({
+      el: '#app',
+      data: {
+        today: ''
+      },
+      created: function() {
+        let todaySet = new Date();
+        let YYYY = todaySet.getFullYear();
+        var MM = ('00' + (todaySet.getMonth() + 1)).slice(-2);
+        var DD = ('00' + todaySet.getDate()).slice(-2);
+        this.today = YYYY + '-' + MM + '-' + DD
+      },
+    })
+  </script>
 </body>
 
 </html>
